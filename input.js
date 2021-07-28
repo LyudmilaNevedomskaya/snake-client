77cb220f5319245556052fcd1ead4e700da178aa
Lyudmila Nevedomskaya <milo4kamoya@gmail.com>
2021-07-28 22:35:15 +0000

    Full code of snake game

---
+++ input.js
@@ -0,0 +1,37 @@
+let connection;
+
+
+
+
+const setupInput = function (conn) {
+  connection = conn;
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  stdin.on("data", handleUserInput);
+
+  return stdin;
+};
+
+const handleUserInput = function (input) {
+
+  if (input === 'w') {
+    connection.write('Move: up');
+  }
+  if (input === 'a') {
+    connection.write('Move: left');
+  }
+  if (input === 'd') {
+    connection.write('Move: right');
+  }
+  if (input === 's') {
+    connection.write('Move: down');
+  }
+  if (input === '\u0003') {
+    process.exit();
+  }
+};
+
+module.exports = setupInput;
--- play.js
+++ play.js
@@ -3,8 +3,9 @@
 //////////////
 
 
-const { stdin } = require('process');
-const connect = require('./client')
+
+const connect = require('./client');
+const setupInput = require('./input');
 
 
 
@@ -31,24 +32,9 @@ const connect = require('./client')
 
 
 console.log("Connecting ...");
-connect();
-
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-
-  stdin.on("data", handleUserInput);
-
-  return stdin;
-};
-
-const handleUserInput = function (input) {
-  if (input === '\u0003') {
-    process.exit();
-  }
-};
+//connect(connect);
 
 
-setupInput();
+
+setupInput(connect());
+
