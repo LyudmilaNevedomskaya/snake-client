8f6d035dd9628fde75b664dcd13238d71496bfd5
Lyudmila Nevedomskaya <milo4kamoya@gmail.com>
2021-07-28 19:42:46 +0000

    Created client.js and moved function from play.js to the client.js

--- play.js
+++ play.js
@@ -1,23 +1,31 @@
-const net = require("net");
+//////////////THIS PART MOVED TO THE client.js
+/////const net = require("net");//////
+//////////////
 
+
+const connect = require('./client')
+
+
+
+//////////////THIS PART MOVED TO THE client.js
 // establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: "135.23.223.133",
-    port: 50542
-  });
+// const connect = function () {
+//   const conn = net.createConnection({
+//     host: "135.23.223.133",
+//     port: 50542
+//   });
 
-  conn.on('data', (data) => {
-    console.log(data);
-  })
+//   conn.on('data', (data) => {
+//     console.log(data);
+//   })
 
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
+//   // interpret incoming data as text
+//   conn.setEncoding("utf8");
 
-  return conn;
+//   return conn;
 
-};
-
+// };
+////////////////////////////////////////////////////////
 
 
 
